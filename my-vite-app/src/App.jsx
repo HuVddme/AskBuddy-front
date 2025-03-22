de11d91975a613f5d9a3f8e5cc5677532fa3eb42
HuVddme <valentine.ezikeoha@bison.howard.edu>
2025-03-22 18:08:11 -0400

    home page

--- my-vite-app/src/App.jsx
+++ my-vite-app/src/App.jsx
@@ -1,14 +1,14 @@
 import React from 'react'
 import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom'
 import LandingPage from './pages/Home'
-import SubmitDocumentPage from './pages/SubmitDocumentPage'
-import AskQuestionPage from './pages/AskQuestionPage'
+import SubmitDocumentPage from './pages/SubmitDocument'
+import AskQuestionPage from './pages/AskQuestion'
 
 const App = () => {
   return (
     <Router>
       <nav style={{ display: 'flex', gap: '1rem', padding: '1rem' }}>
-        <Link to="/">Landing</Link>
+        <Link to="/">Home</Link>
         <Link to="/submit">Submit Document</Link>
         <Link to="/ask">Ask a Question</Link>
       </nav>
